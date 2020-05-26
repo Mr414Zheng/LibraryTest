11958a439adfa5b1a44e1842984ef9f0dfd45080
Mr414Zheng <yy157701297@163.com>
2020-05-26 22:16:14 +0800

    1

---
+++ librarya/src/main/java/com/example/librarya/LibraryA.java
@@ -0,0 +1,15 @@
+package com.example.librarya;
+
+import android.util.Log;
+
+/**
+ * Author: ZhengHuaizhi
+ * Date: 2020/5/26
+ * Description:
+ */
+public class LibraryA {
+
+    public void log() {
+        Log.d("LibraryA", "This is LibraryA!");
+    }
+}
